b5ab635714afed9368beb9389d1d920292741b00
cottonpup <younggyoung.lee1@gmail.com>
2024-03-28 00:20:29 +0900

    feat: useInfiniteQuery 사용

--- src/hooks/queries/useGetCards.ts
+++ src/hooks/queries/useGetCards.ts
@@ -1,16 +1,20 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { Cards } from '@/types/cards';
 
-const getCards = async () => {
-  const res = await axios.get<Cards>(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/cards`);
+const getCards = async ({ pageParam }: { pageParam: number }) => {
+  const res = await axios.get<Cards>(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/cards?page=${pageParam}`);
   return res.data.cards;
 };
 
 export const useGetCards = () => {
-  const { data: cards } = useQuery({
+  const { data: cards } = useInfiniteQuery({
     queryKey: ['cards'],
-    queryFn: () => getCards(),
+    queryFn: (pageParam) => getCards(pageParam),
+    initialPageParam: 0,
+    getNextPageParam: (_lastPage, _allPages, lastPageParam) => {
+      lastPageParam + 1;
+    },
   });
   return { cards };
 };
